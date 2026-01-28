9c50655b985f6a2ef2d8e894bc5ca02bb1671a60
René Rivas <reinaldorrivas@gmail.com>
2026-01-28 18:08:18 -0400

    fh-ts-guide: Add minor comments and some minor change

--- src/bases/helloWorld.ts
+++ src/bases/helloWorld.ts
@@ -8,5 +8,11 @@ export const helloWorld = () => {
     age: 45,
   };
 
+    /*
+     * TypeScript infiere los tipos de datos de una variable lo mejor que puede y,
+     * a partir de allí, trata de mantener esas reglas.
+     */
   hero.age = 50;
+
+  console.log("--- END HELLO WORLD ---");
 };
