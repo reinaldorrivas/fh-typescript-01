620fd6b9fbcb203222f96df1ec93b9a731260144
René Rivas <reinaldorrivas@gmail.com>
2026-01-28 18:09:16 -0400

    fh-ts-guide: Add intro to type number

---
+++ src/bases/typeNumber.ts
@@ -0,0 +1,25 @@
+export const typeNumber = () => {
+  let avengers: number = 10;
+
+  const villians: number = 20;
+
+  /*
+   * Si no se definiera un tipo de dato para avengers, daría un error porque posiblemente
+   * la variable pudiera ser undefined y eso no permite una comparación válida entre números.
+   */
+  if (avengers < villians) {
+    console.log("Estamos en problemas.");
+  } else {
+    console.log("Nos salvamos.");
+  }
+
+  /*
+   * Esto daría NaN que también es considerado un número, así que TypeScript no es infalible.
+   * NaN pudiera ocasionar errores difíciles de rastrear, así que habría qué hacer las validaciones respectivas.
+   */
+  avengers = Number("55A");
+
+  console.log({ avengers });
+
+  console.log("--- END TYPE NUMBER ---");
+};
