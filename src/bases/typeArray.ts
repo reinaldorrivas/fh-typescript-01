e55370028e5919eba010528e9b790d540466986f
René Rivas <reinaldorrivas@gmail.com>
2026-01-29 00:47:38 -0400

    fh-ts-guide: Add type Array intro

---
+++ src/bases/typeArray.ts
@@ -0,0 +1,14 @@
+export const TypeArray = () => {
+  const numbersWithStrings: (string | number)[] = [1, 2, "3", 4, 5];
+  const numbers: number[] = [1, 2, 3, 4, 5];
+
+  console.log({ numbersWithStrings, numbers });
+
+  const villains: string[] = ["Omega Rojo", "Dormammu", "Duende Verde"];
+
+  villains.forEach((villiain) => {
+    console.log(villiain.toUpperCase());
+  });
+
+  console.log("--- END TYPE ARRAY ---");
+};
