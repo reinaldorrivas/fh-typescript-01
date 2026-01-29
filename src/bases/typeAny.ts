fbfe40f501ff2e843b97a8c92ed609409cff492e
René Rivas <reinaldorrivas@gmail.com>
2026-01-29 00:47:12 -0400

    fh-ts-guide: Add type any intro.

---
+++ src/bases/typeAny.ts
@@ -0,0 +1,15 @@
+export const typeAny = () => {
+
+  let a: any = 123;
+
+  a = "René Rivas";
+
+  /* Esto se conoce como casting */
+  console.log((a as string).charAt(0));
+
+  a = 150.54132;
+
+  console.log((a as number).toFixed(2));
+
+  console.log("--- END TYPE ANY ---");
+};
